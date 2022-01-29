fbd0dbb6d890bb6a0e1c4a2312dc7e6396a96619
Matthew Phillips <phillips.matthew829@gmail.com>
2022-01-29 13:46:49 -0600

    Done.

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,13 +1,40 @@
-import React, { useState } from "react";
+import React, {useEffect, useState} from "react";
 import ItemForm from "./ItemForm";
 import Filter from "./Filter";
 import Item from "./Item";
 
-function ShoppingList() {
+// C.R.U.D.: Create, Read, Update, Delete
+
+function ShoppingList(){
   const [selectedCategory, setSelectedCategory] = useState("All");
   const [items, setItems] = useState([]);
 
-  function handleCategoryChange(category) {
+  useEffect(() => {
+    fetch("http://localhost:4000/items") //Default fetch request: GET
+      .then((r) => r.json())
+      .then((items) => setItems(items));
+  }, []);
+
+  function handleDeleteItem(deletedItem){
+    const updatedItems = items.filter((item) => item.id !== deletedItem.id);
+    setItems(updatedItems);
+  }
+
+  function handleUpdateItem(updatedItem){
+    const updatedItems = items.map((item) => {
+      if (item.id === updatedItem.id){
+        return updatedItem;
+      } else{
+        return item;
+      }
+    });
+    setItems(updatedItems);
+  }
+
+  function handleAddItem(newItem){
+    setItems([...items, newItem]);
+  }
+  function handleCategoryChange(category){
     setSelectedCategory(category);
   }
 
@@ -17,16 +44,20 @@ function ShoppingList() {
     return item.category === selectedCategory;
   });
 
-  return (
+  return(
     <div className="ShoppingList">
-      <ItemForm />
+      <ItemForm onAddItem={handleAddItem} />
       <Filter
         category={selectedCategory}
         onCategoryChange={handleCategoryChange}
       />
       <ul className="Items">
         {itemsToDisplay.map((item) => (
-          <Item key={item.id} item={item} />
+          <Item 
+          key={item.id} 
+          item={item} 
+          onUpdateItem={handleUpdateItem}
+          onDeleteItem={handleDeleteItem} />
         ))}
       </ul>
     </div>
